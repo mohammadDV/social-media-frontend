11bdd422edfea00a0a9df6c7d5f9aa28f359bfdc
Mohammad daneshmand <81333964+mohammadDV@users.noreply.github.com>
2024-03-16 17:52:07 +0300

    add the warning login

--- src/components/profile/components/status/StatusWallComponent.vue
+++ src/components/profile/components/status/StatusWallComponent.vue
@@ -4,7 +4,20 @@
   import { onMounted, ref, watch, defineProps } from 'vue';
   import statusCardComponent from './StatusCardComponent.vue'
   import { useRoute } from 'vue-router';
-  import VTButton from '@/elements/VTButton'; 
+  import VTButton from '@/elements/VTButton';
+  import VTModal from '@/elements/VTModal.vue'
+  import { useAuthStore } from '@/stores/auth.ts';
+  import { useToast } from "vue-toast-notification";
+  import VTTextArea from '@/elements/VTTextArea';
+
+  
+  const authStore = useAuthStore();
+
+  const isModalVisible = ref(false);
+  const message = ref('');
+  const showModal = () => {
+    isModalVisible.value = true;
+  } 
 
     const props = defineProps({
         userId: {
@@ -73,6 +86,33 @@
     page.value = 1;
     getStatuses();
   }
+
+
+  const closeModal = () => {
+    message.value = '';
+    isModalVisible.value = false;
+  };
+
+
+  const $toast = useToast();
+  const send = () => {
+    if (route.params.id) {
+        useApi().post(`/api/profile/chats/${route.params.id}`, {
+            message: message.value, 
+        })
+        .then((response) => {
+        if (response.data.status) {
+            $toast.success(response.data.message);
+            closeModal()
+        }
+        })
+        .catch(error => {
+            if (error.response.data.status == 0) {
+                $toast.error(error.response.data.message);
+            }
+        });
+    }
+  };
     
     onMounted(() => {
         getStatuses();
@@ -81,7 +121,7 @@
 </script>
 
 <template>
-    <div v-if="route?.params?.id?.length > 0" class="card vt-news-card breadcrumb-card mb-3">
+    <!-- <div v-if="route?.params?.id?.length > 0" class="card vt-news-card breadcrumb-card mb-3">
         <div class="card-body">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
@@ -93,7 +133,16 @@
                 </ol>
             </nav>
         </div>
-    </div>
+    </div> -->
+    <div v-if="route?.params?.id?.length > 0 && route?.params?.id != authStore.user.id" class="card posting-card">
+            <div class="card-body">
+                <div class="post-btn-container">
+                <button @click="showModal" class="btn input-is-btn">
+                    {{ $t('site.Send a private messasge') }}
+                </button>
+                </div>
+            </div>
+        </div>
     <div class="tweet-reel">
         <div v-for="(status, index) in items" :key="index" class="card tweet-card">
             <statusCardComponent 
@@ -114,4 +163,28 @@
             </VTButton> 
         </div>
     </div>
+
+
+    <VTModal v-if="isModalVisible" @close="closeModal" :title="$t('site.Send a private messasge')">
+        <div class="w-[300px] sm:w-[600px]">
+            <div class="mb-3">
+                <VTTextArea
+                    name="text"
+                    rows="4"
+                    v-model="message"
+                    :disabled="false"
+                    request-name="ChatRequest"
+                    :placeholder="$t('site.Send a private messasge')"
+                    />
+            </div>
+            <VTButton 
+                class="btn btn-outline-secondary btn-sm" 
+                size="medium"
+                color="primary"  
+                @click="send()">
+                {{ $t('site.Save') }}
+            </VTButton>  
+            
+        </div>
+    </VTModal>
 </template>
